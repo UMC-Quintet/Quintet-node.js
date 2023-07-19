a286276f1049d3a3cda75c244f51177f90449473
Jayde1012 <ariha1982@naver.com>
2023-07-20 02:30:43 +0900

    first commit(jayde)

---
+++ src/provider/guideProvider.js
@@ -0,0 +1,9 @@
+const { pool } = require("../../config/database");
+
+const guideDao = require("../dao/guideDao");
+
+// Provider: Read 비즈니스 로직 처리
+/*
+exports.guideApi = async function (param) {
+};
+*/
---
+++ src/route/guideRoute.js
@@ -0,0 +1,5 @@
+module.exports = function (app) {
+    const guide = require('../controller/guideController');
+
+    //app.get(), app.post() ...
+}
