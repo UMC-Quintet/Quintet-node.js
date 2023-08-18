5204f7aa050c89ec64c6216344c041840a91d05b
jayde <ariha1982@naver.com>
2023-08-19 06:28:26 +0900

    Refactor - 날짜 함수 수정

--- src/controller/recordController.js
+++ src/controller/recordController.js
@@ -1,16 +1,25 @@
 const baseResponse = require("../../config/baseResponseStatus");
 const {response, errResponse} = require("../../config/response");
 const recordService = require("../service/recordService");
-const recordProvider = require("../provider/recordProvider");
+const recordProvider = require("../provider/recordProvider")
+
+function getTodayDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
+
+    return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+}
 
 exports.postTodayChecks = async function (req, res) {
     //const user_id = req.user.user_id;
     const {user_id, work_deg, health_deg, family_deg, relationship_deg, money_deg} = req.body;
 
-    const today = new Date();
-    console.log(today);
+    const todayDate = getTodayDate();
+    console.log(todayDate);
 
-    const postTodayChecksResult = await recordService.todayChecks(user_id, today, work_deg, health_deg, family_deg, relationship_deg, money_deg);
+    const postTodayChecksResult = await recordService.todayChecks(user_id, todayDate, work_deg, health_deg, family_deg, relationship_deg, money_deg);
 
     return res.send(postTodayChecksResult);
 };
@@ -19,10 +28,10 @@ exports.patchTodayRecord = async function (req, res) {
     //const user_id = req.user.user_id;
     const {user_id, work_doc, health_doc, family_doc, relationship_doc, money_doc } = req.body;
 
-    const today = new Date();
-    console.log(today);
+    const todayDate = getTodayDate();
+    console.log(todayDate);
 
-    const patchTodayRecordsResult = await recordService.todayRecords(user_id, today, work_doc, health_doc, family_doc, relationship_doc, money_doc);
+    const patchTodayRecordsResult = await recordService.todayRecords(user_id, todayDate, work_doc, health_doc, family_doc, relationship_doc, money_doc);
 
     return res.send(patchTodayRecordsResult);
 };
