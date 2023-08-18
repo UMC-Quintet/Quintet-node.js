8ad93c188247ea03bfefeb97fe8a777e2e42b1d5
jayde <ariha1982@naver.com>
2023-08-19 07:18:37 +0900

    Refactor - KST 로직 삭제

--- src/controller/staticController.js
+++ src/controller/staticController.js
@@ -1,25 +1,12 @@
 const staticProvider = require("../provider/staticProvider");
 const baseResponse = require("../../config/baseResponseStatus");
 const {response, errResponse} = require("../../config/response");
-const moment = require('moment-timezone');
-function getWeekRange() {
-    const KST = moment().tz('Asia/Seoul');
-    const day = KST.day(); // 요일 (0: 일요일, 1: 월요일, ..., 6: 토요일)
-    const diff = KST.date() - day + (day === 0 ? -6 : 0); // 해당 주의 첫 날짜
-
-    const startOfWeek = KST.date(diff).format('YYYY-MM-DD');
-    const endOfWeek = KST.date(diff+6).format('YYYY-MM-DD'); // 해당 주의 마지막 날짜
-
-    return { start: startOfWeek, end: endOfWeek };
-}
 
 exports.getWeeklyStatic = async function (req, res) {
     //const user_id = req.user.user_id;
-    const user_id = req.query.user_id;
-    const { start, end } = getWeekRange();
-    console.log(start);
+    const { user_id, startDate, endDate } = req.query;
 
-    const weeklyStaticResult = await staticProvider.weeklySum(user_id, start, end);
+    const weeklyStaticResult = await staticProvider.weeklySum(user_id, startDate, endDate);
 
     return res.send(response(baseResponse.SUCCESS, weeklyStaticResult));
 };
