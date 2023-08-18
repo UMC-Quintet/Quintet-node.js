c47f3cbb882584063e000c71be4d8c6a669930c9
jayde <ariha1982@naver.com>
2023-08-19 06:13:20 +0900

    Refactor - 날짜 부분 수정

--- src/controller/staticController.js
+++ src/controller/staticController.js
@@ -1,10 +1,22 @@
 const staticProvider = require("../provider/staticProvider");
 const baseResponse = require("../../config/baseResponseStatus");
 const {response, errResponse} = require("../../config/response");
+function getWeekRange() {
+    const date = new Date();
+    const day = date.getDay(); // 요일 (0: 일요일, 1: 월요일, ..., 6: 토요일)
+    const diff = date.getDate() - day + (day === 0 ? -6 : 0); // 해당 주의 첫 날짜
+    const startOfWeek = new Date(date.setDate(diff));
+
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(endOfWeek.getDate() + 6); // 해당 주의 마지막 날짜
+
+    return { start: startOfWeek, end: endOfWeek };
+}
 
 exports.getWeeklyStatic = async function (req, res) {
     //const user_id = req.user.user_id;
-    const {user_id, startDate, endDate} = req.query;
+    const user_id = req.query.user_id;
+    const {startDate, endDate} = getWeekRange();
 
     const weeklyStaticResult = await staticProvider.weeklySum(user_id, startDate, endDate);
 
