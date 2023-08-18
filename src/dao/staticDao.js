c6a3e4a66aed46259ddbd36bd1b1cb3aa637e9ab
jayde <ariha1982@naver.com>
2023-08-19 02:16:57 +0900

    Refactor - db에 맞게 변수 이름 수정

--- src/dao/staticDao.js
+++ src/dao/staticDao.js
@@ -1,11 +1,7 @@
-/*
-async function guideApi(connection) {
-}
-*/
 async function weeklySum(connection, user_id, startDate, endDate) {
     const weeklySumQuery = `SELECT sum(work_deg) as work_deg, sum(health_deg) as health_deg, sum(family_deg) as family_deg, sum(relationship_deg) as relationship_deg, sum(money_deg) as money_deg
                             FROM document
-                            WHERE user_id = ? and quintet_dt between ? and ?;`;
+                            WHERE user_id = ? and date between ? and ?;`;
     const weeklySumRow = await connection.query(weeklySumQuery, [user_id, startDate, endDate]);
     return weeklySumRow[0];
 }
@@ -13,7 +9,7 @@ async function weeklySum(connection, user_id, startDate, endDate) {
 async function monthlySum(connection, user_id, year, month) {
     const monthlySumQuery = `SELECT sum(work_deg) as work_deg, sum(health_deg) as health_deg, sum(family_deg) as family_deg, sum(relationship_deg) as relationship_deg, sum(money_deg) as money_deg
                             FROM document
-                            WHERE user_id = ? and year(quintet_dt) = ? and month(quintet_dt) = ?`;
+                            WHERE user_id = ? and year(date) = ? and month(date) = ?`;
     const monthlySumRow = await connection.query(monthlySumQuery, [user_id, year, month]);
     return monthlySumRow[0];
 }
@@ -21,7 +17,7 @@ async function monthlySum(connection, user_id, year, month) {
 async function annualSum(connection, user_id, year) {
     const annualSumQuery = `SELECT sum(work_deg) as work_deg, sum(health_deg) as health_deg, sum(family_deg) as family_deg, sum(relationship_deg) as relationship_deg, sum(money_deg) as money_deg
                             FROM document
-                            WHERE user_id = ? and year(quintet_dt) = ?;`;
+                            WHERE user_id = ? and year(date) = ?;`;
     const annualSumRow = await connection.query(annualSumQuery, [user_id, year]);
     return annualSumRow[0];
 }
