ae8d3b7e462a638bba05b4174caf1467f74e2881
jayde <ariha1982@naver.com>
2023-08-21 19:31:50 +0900

    Refactor - home api 수정

--- src/dao/homeDao.js
+++ src/dao/homeDao.js
@@ -2,7 +2,7 @@
 //해당 주 퀸텟 기록을 배열로 반환
 async function selectQuintet(connection, userId, startDate, endDate) {
     const selectQuintetQuery = `
-                SELECT user_id, date, work_deg, health_deg, family_deg, relationship_deg, money_deg
+                SELECT date, work_deg, health_deg, family_deg, relationship_deg, money_deg
                 FROM document
                 WHERE user_id = ? and date between ? and ?`;
     return await connection.query(selectQuintetQuery, [userId, startDate, endDate]);
