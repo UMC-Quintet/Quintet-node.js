f878f56c5394b3c6854e1d966b4e2f21dbd00848
jayde <ariha1982@naver.com>
2023-08-22 15:21:56 +0900

    Fix - home api 수정

--- src/controller/recordController.js
+++ src/controller/recordController.js
@@ -9,8 +9,8 @@ exports.postTodayChecks = async function (req, res) {
     const {user_id, work_deg, health_deg, family_deg, relationship_deg, money_deg} = req.body;
 
     const KST = moment().tz('Asia/Seoul');
-    const todayDate = KST.format('YYYY-MM-DD');
-    console.log(todayDate+1);
+    const todayDate = KST.format('YYYY-MM-DD').toString();
+    console.log(todayDate);
 
     const checkDuplicate = await recordProvider.checkDuplicateData(user_id, todayDate);
 
