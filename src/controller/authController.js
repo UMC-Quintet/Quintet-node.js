b22f179bf79441bef2d4c8e404bd94d9f97f03f7
jayde <ariha1982@naver.com>
2023-11-22 22:26:32 +0900

    [FEAT] 로그인시 토큰 제공하도록 변경

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,6 +1,9 @@
 const {response, errResponse} = require("../../config/response");
 const baseResponse = require("../../config/baseResponseStatus");
 const {OAuth2Client} = require('google-auth-library');
+const appleAuth = require('apple-auth');
+const appleConfig = require('../../config/apple.json');
+const auth = new appleAuth(appleConfig, appleConfig.private_key_path);
 const userProvider = require("../provider/userProvider");
 const userService = require("../service/userService");
 const dotenv = require("dotenv");
@@ -28,10 +31,45 @@ exports.loginGoogleUser = async function (req, res) {
         exUser = await userProvider.getUserBySnsId(googleId, 'google');
         const tokens = await userProvider.getGoogleToken(exUser);
 
-        return res.send(response(baseResponse.SUCCESS, tokens));
+        return res.header('Authorization', `Bearer ${tokens}`).send(response(baseResponse.SUCCESS));
 
     } catch (e) { //유효성 검증에 실패했을 경우
         console.log(e);
         return res.send(errResponse(baseResponse.INVALID_TOKEN));
     }
 };
+
+exports.loginAppleUser = async function (req, res) {
+    const idToken = req.body.idToken;
+    try {
+        const decodedToken = await auth.verifyIdToken(idToken, {
+            audience: 'client_id',
+            ignoreExpiration: true,
+        });
+        console.log(decodedToken);
+    } catch (e) {
+        console.log(e);
+        return res.send(errResponse(baseResponse.INVALID_TOKEN));
+    }
+}
+
+exports.loginTestUser = async function (req, res) {
+    const {testSnsId, name, email} = req.body;
+    try {
+        let exUser = await userProvider.getUserBySnsId(testSnsId, 'test');
+        if(exUser){
+            console.log(exUser); //해당 유저 존재 시 콘솔 출력
+        } else {
+            console.log("해당 유저 없음");
+            await userService.insertNewUser(name, email, 'test', null, testSnsId);
+        }
+
+        exUser = await userProvider.getUserBySnsId(testSnsId, 'test');
+        const tokens = await userProvider.getGoogleToken(exUser);
+
+        return res.header('Authorization', `Bearer ${tokens}`).send(response(baseResponse.SUCCESS));
+    } catch (e) {
+        console.log(e);
+        return res.send(errResponse(baseResponse.INVALID_TOKEN));
+    }
+}
