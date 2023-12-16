f00baa729d4c7993b30e083c87e4da5769380099
Jayde1012 <ariha1982@naver.com>
2023-12-16 22:47:40 +0900

    [FEAT] elaticache redis 연동 설정

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -1,5 +1,12 @@
 const redis = require('redis');
-const client = redis.createClient({legacyMode: true});
+const dotenv = require('dotenv');
+dotenv.config();
+
+const client = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    legacyMode: true
+});
 
 client.on('connect', () => {
     console.log('redisConfig 연결에 성공했습니다.');
