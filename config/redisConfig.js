affb54b00c5893112df96dce902c83eb4646a4b7
Jayde1012 <ariha1982@naver.com>
2023-12-16 23:52:14 +0900

    [ETC] env 설정 변경

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const client = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
+    host: process.env["REDIS_HOST "],
+    port: process.env["REDIS_PORT "],
     legacyMode: true
 });
 
